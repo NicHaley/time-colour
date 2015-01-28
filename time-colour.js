2e1cfef873bcec07f743f9d9d8616519dcf4e41e
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2015-01-28 16:34:43 -0500

    Changed colour every second

--- time-colour.js
+++ time-colour.js
@@ -2,13 +2,19 @@ Meteor.startup(function() {
 
   setInterval(function() {
 
-  var today = new Date();
-  var hour = today.getHours();
-  var minute = today.getMinutes();
-  var second = today.getSeconds();
+    var today = new Date();
+    var hour = today.getHours();
+    var minute = today.getMinutes();
+    var second = today.getSeconds();
 
-  $('#time').html(hour + ":" + minute + ":" + second);
-  $('.bg-container').css("background-color", "red");
+    hourColour = Math.round((hour / 23) * 255);
+
+    minuteColour = Math.round((minute / 59) * 255);
+    secondColour = Math.round((second / 59) * 255);
+    console.log(secondColour);
+
+    $('#time').html(hour + ":" + minute + ":" + second);
+    $('.bg-container').css("background-color", "rgb(" + hourColour +", " + minuteColour + ", " + secondColour + ")");
 
   }, 500);
 
