9630a4400ed9d264f79e8a4d2a460dfd64d68934
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2015-02-04 12:43:57 -0500

    Started setting up todo submit

---
+++ client/templates/todos/todo_submit.js
@@ -0,0 +1,13 @@
+// Template.todoSubmit.events({
+// 	'submit form': function(event, template) {
+// 		event.preventDefault();
+
+// 		var $body = $(event.target).find('[name=body');
+// 		var todo = {
+// 			body: $body.val(),
+// 			todoId: template.data._id
+// 		};
+
+// 		Meteor.call('')
+// 	}
+// });
