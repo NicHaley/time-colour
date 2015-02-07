00fe64a294e21f02ce83fb0d2bf85d8d53673208
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2015-02-07 15:36:48 -0500

    Set up todos

--- server/publications.js
+++ server/publications.js
@@ -1,4 +1,8 @@
 Meteor.publish('todos', function() {
+
+	// Assign current user to a variable
 	var currentUserId = this.userId;
+
+	// Publish todos where the Todo userId attribtue is equal to the currentUserId
 	return Todos.find({userId: currentUserId});
 });
