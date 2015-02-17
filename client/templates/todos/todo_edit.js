f5aca76c1bf865573cf5612759a43a84b0f296bb
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2015-02-16 23:45:01 -0500

    Filled out todo_edit.js

---
+++ client/templates/todos/todo_edit.js
@@ -0,0 +1,16 @@
+Template.todoEdit.events({
+	'keypress #body': function(event, template) {
+		if(event.which === 13) {
+			event.preventDefault();
+
+			var currentTodoId = this._id;
+
+			var $body = template.find("#body").value;
+			var todo = {
+				body: $body
+			};
+
+			Todos.update(currentTodoId, {$set: todo});
+		}
+	}
+});
