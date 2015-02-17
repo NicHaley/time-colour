df0b42450c61038723f2602edc8d317e643e81b3
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2015-02-17 11:24:48 -0500

    Changed body id to a class to allow for multiple updatable todos

--- client/templates/todos/todo_edit.js
+++ client/templates/todos/todo_edit.js
@@ -1,11 +1,11 @@
 Template.todoEdit.events({
-	'keypress #body': function(event, template) {
+	'keypress .body': function(event, template) {
 		if(event.which === 13) {
 			event.preventDefault();
 
 			var currentTodoId = this._id;
 
-			var $body = template.find("#body").value;
+			var $body = template.find(".body", this).value;
 			var todo = {
 				body: $body
 			};
