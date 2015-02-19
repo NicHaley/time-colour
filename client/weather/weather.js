9dd2bd551e3c464c8db7fa861a1276d069d73562
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2015-02-19 12:37:30 -0500

    Styled weater div

--- client/weather/weather.js
+++ client/weather/weather.js
@@ -1,16 +1,23 @@
 Meteor.startup(function() {
-	$.simpleWeather({
-		location: 'Toronto, ON',
-		woeid: '',
-		unit: 'c',
-		success: function(weather) {
 
-			html = '<h2><i class="icon-' + weather.code + '"></i> ' + weather.temp + '&deg;' + weather.units.temp + '</h2>';
-
-			$("#weather").html(html);
-		},
-		error: function(error) {
-			$("#weather").html('<p>'+error+'</p>');
-		}
+	navigator.geolocation.getCurrentPosition(function(position) {
+		loadWeather(position.coords.latitude + ',' + position.coords.longitude);
 	});
+
+	function loadWeather(location) {
+	  $.simpleWeather({
+	    location: location,
+	    woeid: '',
+			unit: 'c',
+			success: function(weather) {
+
+				html = '<i class="icon-' + weather.code + '"></i> <h6>' + weather.temp + '&deg;' + weather.units.temp + '</h6>';
+
+				$("#weather").html(html);
+			},
+			error: function(error) {
+				$("#weather").html('<p>'+error+'</p>');
+			}
+		});
+	}
 });
